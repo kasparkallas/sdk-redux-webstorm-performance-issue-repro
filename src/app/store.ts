92677c7fb1f10b87bf38787b2d54537d3d31d918
Kaspar Kallas <kaspar@superfluid.finance>
2022-02-07 17:46:06 +0200

    Add performance issue related code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,16 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import {
+  allSubgraphSliceEndpoints,
+  createApiWithReactHooks,
+  initializeSubgraphSlice
+} from "@superfluid-finance/sdk-redux";
+
+// This is initialization of the SDK that causes issues for Webstorm. Visual Studio Code has no issues.
+const sfSubgraph = initializeSubgraphSlice(createApiWithReactHooks).injectEndpoints(allSubgraphSliceEndpoints);
+
+// Code completion *especially* dies here. It should offer many "useSomethingQuery" & "usLazySomethingQuery but shows a loader forever instead...
+// const foo = sfSubgraph.use
 
 export const store = configureStore({
   reducer: {
